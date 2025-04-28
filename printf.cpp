d74ecd75a47d695a077ad9ca55ff0f5a06399a2d
以南爬行 <jh13598309316@163.com>
2025-04-28 14:22:59 +0800

    Update printf.cpp

--- printf.cpp
+++ printf.cpp
@@ -1,10 +1,10 @@
-#include<stdio.h>
-
-int main(){
-    int a,b;
-    scanf("%d%d", &a , &b);
-    printf("a=%d,b=%d\n",a,b);
-    
-    
-    return 0;
-}
+#include<stdio.h>  
+int main(){     
+int n1,n2;     
+n1 = printf("hello world\n");
+printf("%d\n",'\n');//输出换行符ASCII码的值
+int a = 123;
+n2 = printf("hello world :%d\n",a);
+printf("n1 = %d,n2 = %d\n",n1,n2);//输出后面有个换行符\n,它本质上也是一个字符
+return 0;
+} 
