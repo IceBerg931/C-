2b739b67212f7214498803916ff9c5b45c77526f
以南爬行 <jh13598309316@163.com>
2025-04-28 13:40:09 +0800

    Add files via upload

--- printf.cpp
+++ printf.cpp
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
 int main(){
-    printf("hello world\n");
+    int n1,n2;
+    n1 = printf("hello world\n");
     int a = 123;
-    printf("hello world :%d\n",a);
-    
+    n2 = printf("hello world :%d\n",a);
+    printf("n1 = %d,n2 = %d\n",n1,n2);//输出后面有个换行符\n,它本质上也是一个字符
     return 0;    
     
     }
