72144f56561f2da4222df5e46d04ad4bc551e8e4
以南爬行 <jh13598309316@163.com>
2025-04-28 14:28:32 +0800

    Add files via upload

--- scanf.cpp
+++ scanf.cpp
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 int main(){
-    int a,b;
-    scanf("%d%d", &a , &b);
-    printf("a=%d,b=%d\n",a,b);
-    
+    int a,b,n;
+    n = scanf("%d%d", &a , &b);
+    printf("a = %d,b = %d\n",a,b);
+    printf("n = %d\n", n);
     
     return 0;
 }
