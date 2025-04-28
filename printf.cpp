8c31a62d23510e1aaf689f31c8b41c80f3a3c867
以南爬行 <jh13598309316@163.com>
2025-04-28 13:44:51 +0800

    Add files via upload

--- printf.cpp
+++ printf.cpp
@@ -3,6 +3,7 @@
 int main(){
     int n1,n2;
     n1 = printf("hello world\n");
+    printf("%d\n",'\n');//输出换行符ASCII码的值
     int a = 123;
     n2 = printf("hello world :%d\n",a);
     printf("n1 = %d,n2 = %d\n",n1,n2);//输出后面有个换行符\n,它本质上也是一个字符
